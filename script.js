4c43ced497125c06ae7246637fee2e6e794e304d
Matt Hulker <matthulker@icloud.com>
2024-07-19 13:24:07 -0500

    add the mouseover function for 'drawing'

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const container = document.querySelector(".container")
 for (let i = 0; i < 256; i++){
    const div = document.createElement('div')
    div.classList.add('item')
+   div.addEventListener("mouseover", function(e) {
+    e.target.style.background = "black"
+   })
     container.appendChild(div)
 }
